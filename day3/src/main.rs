e928780a3bfbe9a09c7bb46818e78f65df0df8bd
Son Luong Ngoc <sluongng@gmail.com>
2022-12-03 11:13:17 +0100

    rewrite in functional style

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn main() {
-    let lines = "
+    let sum: u32 = "
 vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
@@ -11,49 +11,55 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 "
     .trim()
-    .split("\n");
-
-    let mut badges = vec![];
-    let mut group = vec![];
-    let mut pos = 0;
-    for line in lines {
-        group.insert(group.len(), line);
-
-        if pos % 3 == 2 {
-            let mut item_count = HashMap::new();
-
-            'group_loop: for line in group {
-                for c in line.chars().collect::<HashSet<char>>() {
-                    item_count
-                        .entry(c)
-                        .and_modify(|counter| *counter += 1)
-                        .or_insert(1);
-
-                    match item_count.get(&c) {
-                        Some(counter) => {
-                            if *counter >= 3 {
-                                badges.insert(badges.len(), c);
-                                break 'group_loop;
-                            }
-                        }
-                        None => {}
-                    }
+    .split("\n")
+    // Fold the lines into a big group,
+    // where each group is a vector of 3 lines.
+    .fold(vec![], |mut groups, line| {
+        match groups.last_mut() {
+            None => groups.push(vec![line]),
+            Some(g) => {
+                if g.len() >= 3 {
+                    groups.push(vec![line]);
+                } else {
+                    g.push(line);
                 }
             }
-
-            group = vec![];
         }
 
-        pos += 1;
-    }
+        groups
+    })
+    .iter()
+    // Find a common badge between 3 lines inside a group
+    .map(|g| {
+        let mut item_count = HashMap::new();
+        let mut res = 'a';
 
-    let mut sum = 0;
-    for b in badges {
-        sum += b.to_digit(36).unwrap() - 'a'.to_digit(36).unwrap() + 1;
-        if b.is_uppercase() {
-            sum += 26;
+        // TODO: rewrite in functional style?
+        'group_loop: for line in g {
+            for c in line.chars().collect::<HashSet<char>>() {
+                item_count
+                    .entry(c)
+                    .and_modify(|counter| *counter += 1)
+                    .or_insert(1);
+
+                match item_count.get(&c) {
+                    Some(counter) => {
+                        if *counter >= 3 {
+                            res = c;
+                            break 'group_loop;
+                        }
+                    }
+                    None => {}
+                }
+            }
         }
-    }
+
+        res
+    })
+    // calculate value of each badge
+    .map(|b| (b, b.to_digit(36).unwrap() - 'a'.to_digit(36).unwrap() + 1))
+    .map(|(b, val)| if b.is_uppercase() { val + 26 } else { val })
+    .sum();
 
     println!("{}", sum);
 }
