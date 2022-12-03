f8d44bca1f6e6a41f534f8a5dd6f152e00882c4b
Son Luong Ngoc <sluongng@gmail.com>
2022-12-03 17:10:45 +0100

    move logic to standalong fn(s)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,38 +11,53 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 "
     .trim()
     .split("\n")
-    // Fold the lines into a big group,
-    // where each group is a vector of 3 lines.
-    .fold(vec![], |mut groups, line| {
-        match groups.last_mut() {
-            None => groups.push(vec![line]),
-            Some(g) => {
-                if g.len() >= 3 {
-                    groups.push(vec![line]);
-                } else {
-                    g.push(line);
-                }
-            }
-        }
-
-        groups
-    })
+    .fold(vec![], groups_of_three)
     .iter()
-    // Find a common badge between 3 lines inside a group
-    .map(|g| {
-        g.iter()
-            .map(|line| line.chars().collect::<HashSet<char>>())
-            .reduce(|acc, h| acc.intersection(&h).map(|h| *h).collect())
-            .unwrap()
-            .iter()
-            .next()
-            .unwrap()
-            .clone()
-    })
-    // calculate value of each badge
-    .map(|b| (b, b.to_digit(36).unwrap() - 'a'.to_digit(36).unwrap() + 1))
-    .map(|(b, val)| if b.is_uppercase() { val + 26 } else { val })
+    .map(group_to_badge)
+    .map(badge_to_priority)
     .sum();
 
     println!("{}", sum);
+}
+
+// Fold the lines into a big group,
+// where each group is a vector of 3 lines.
+fn groups_of_three<'a, 'b>(mut groups: Vec<Vec<&'a str>>, line: &'b str) -> Vec<Vec<&'a str>>
+where
+    'b: 'a,
+{
+    match groups.last_mut() {
+        None => groups.push(vec![line]),
+        Some(g) => {
+            if g.len() >= 3 {
+                groups.push(vec![line]);
+            } else {
+                g.push(line);
+            }
+        }
+    }
+
+    groups
+}
+
+// Find a common badge between 3 lines inside a group
+fn group_to_badge(group: &Vec<&str>) -> char {
+    group
+        .iter()
+        .map(|line| line.chars().collect::<HashSet<char>>())
+        .reduce(|acc, h| acc.intersection(&h).map(|h| *h).collect())
+        .unwrap()
+        .iter()
+        .next()
+        .unwrap()
+        .clone()
+}
+
+fn badge_to_priority(b: char) -> u32 {
+    let p = b.to_digit(36).unwrap() - 'a'.to_digit(36).unwrap() + 1;
+    if b.is_uppercase() {
+        p + 26
+    } else {
+        p
+    }
 }
