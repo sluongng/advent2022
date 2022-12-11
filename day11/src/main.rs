bbc7273f2f9f36cb92155f9077079dca293a5426
Son Luong Ngoc <sluongng@gmail.com>
2022-12-11 13:57:52 +0100

    add day11 p1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,6 +1,5 @@
-use std::fs;
-
 use std::collections::VecDeque;
+use std::fs;
 
 struct Monkey {
     name: String,
@@ -24,30 +23,34 @@ impl Monkey {
     }
 }
 
-fn block_to_pairs(block: &str) -> Vec<(String, String)> {
+fn block_to_pairs(block: &str) -> Vec<(String, Option<String>)> {
     block
         .split("\n")
         .map(|line| {
             let mut temp = line.split(":").map(str::trim).map(|s| s.into());
 
-            (temp.next().unwrap(), temp.next().unwrap())
+            (temp.next().unwrap(), temp.next())
         })
         .collect::<Vec<_>>()
 }
 
-fn to_monkey(lines: Vec<(String, String)>) -> Monkey {
+fn to_monkey(lines: Vec<(String, Option<String>)>) -> Monkey {
     // handle first line for Monkey's name
     let mut m = Monkey::new(lines[0].0.clone());
 
     lines.iter().skip(1).for_each(|l| match l.0.as_str() {
         "Starting items" => {
-            l.1.split(", ")
+            l.1.as_ref()
+                .unwrap()
+                .split(", ")
                 .map(|i| i.parse::<i32>().unwrap())
                 .for_each(|i| m.items.push_back(i))
         }
         "Operation" => {
             let tokens =
-                l.1.split(" = ")
+                l.1.as_ref()
+                    .unwrap()
+                    .split(" = ")
                     .nth(1)
                     .unwrap()
                     .split(" ")
@@ -79,13 +82,34 @@ fn to_monkey(lines: Vec<(String, String)>) -> Monkey {
             m.op = Box::new(op);
         }
         "Test" => {
-            m.divisible_test_val = l.1.split(" ").last().unwrap().parse::<i32>().unwrap();
+            m.divisible_test_val =
+                l.1.as_ref()
+                    .unwrap()
+                    .split(" ")
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap();
         }
         "If true" => {
-            m.test_pass_monkey = l.1.split(" ").last().unwrap().parse::<i32>().unwrap();
+            m.test_pass_monkey =
+                l.1.as_ref()
+                    .unwrap()
+                    .split(" ")
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap();
         }
         "If false" => {
-            m.test_fail_monkey = l.1.split(" ").last().unwrap().parse::<i32>().unwrap();
+            m.test_fail_monkey =
+                l.1.as_ref()
+                    .unwrap()
+                    .split(" ")
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap();
         }
         _ => (),
     });
@@ -94,17 +118,42 @@ fn to_monkey(lines: Vec<(String, String)>) -> Monkey {
 }
 
 fn main() {
-    let input = fs::read_to_string("./src/input1.txt").unwrap();
-    let monkeys = input
+    let input = fs::read_to_string("./src/input2.txt").unwrap();
+    let mut monkeys = input
         .as_str()
         .split("\n\n")
         .map(block_to_pairs)
         .map(to_monkey)
         .collect::<Vec<_>>();
 
-    for m in monkeys {
-        println!("{}", m.name);
-        println!("  Items: {:?}", m.items);
-        println!("  Ops: {}", (m.op)(1));
+    let mut monkey_business = vec![0; monkeys.len()];
+    for iter in 0..20 {
+        println!("\nIter: {}", iter);
+
+        for i in 0..monkeys.len() {
+            monkey_business[i] += monkeys[i].items.len();
+
+            while let Some(item) = monkeys[i].items.pop_front() {
+                let level = (monkeys[i].op)(item) / 3;
+
+                let mnk_idx = match level % monkeys[i].divisible_test_val {
+                    0 => monkeys[i].test_pass_monkey as usize,
+                    _ => monkeys[i].test_fail_monkey as usize,
+                };
+                monkeys[mnk_idx].items.push_back(level);
+            }
+        }
+
+        for m in monkeys.iter() {
+            println!("{}: {:?}", m.name, m.items);
+        }
+        println!("bzn: {:?}", monkey_business);
     }
+
+    monkey_business.sort();
+
+    println!(
+        "Total bzn: {}",
+        monkey_business.pop().unwrap() * monkey_business.pop().unwrap()
+    );
 }
