b72b45fd9dce38e82060569055c609ef8866e65a
Son Luong Ngoc <sluongng@gmail.com>
2022-12-13 20:36:58 +0100

    minor style fix

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -31,7 +31,6 @@ fn main() {
                 .map(|line| serde_json::from_str(line).expect("line is not json"))
         })
         .collect();
-
     all_lines.extend(vec![2, 6].iter().map(|i| json!(i)));
     all_lines.sort_by(|a, b| is_ordered((a.clone(), b.clone())));
 
@@ -50,11 +49,7 @@ fn main() {
 
 fn is_ordered((left, right): (Value, Value)) -> Ordering {
     match (left, right) {
-        (Value::Number(l), Value::Number(r)) => {
-            let (l64, r64) = (l.as_i64().unwrap(), r.as_i64().unwrap());
-
-            l64.cmp(&r64)
-        }
+        (Value::Number(l), Value::Number(r)) => l.as_i64().unwrap().cmp(&(r.as_i64().unwrap())),
         (Value::Number(l), Value::Array(r)) => is_ordered((json!([l]), json!(r))),
         (Value::Array(l), Value::Number(r)) => is_ordered((json!(l), json!([r]))),
         (Value::Array(l), Value::Array(r)) => match (l.len(), r.len()) {
