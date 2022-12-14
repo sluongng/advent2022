91cfc6c9e83a9b9125c64c8208fe020929eac4cb
Son Luong Ngoc <sluongng@gmail.com>
2022-12-14 10:04:16 +0100

    d14: Remove the loop

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -5,90 +5,88 @@ fn main() {
     let input1 = fs::read_to_string("src/input1.txt").unwrap();
     let input2 = fs::read_to_string("src/input2.txt").unwrap();
 
-    for input in vec![input2] {
-        let cave = input
-            .as_str()
-            .trim()
-            .split('\n')
-            .map(|line| {
-                line.split(" -> ")
-                    .map(|coord| {
-                        let mut vec = coord
-                            .split(',')
-                            .map(|number| number.parse::<i32>().unwrap());
+    let cave = input2
+        .as_str()
+        .trim()
+        .split('\n')
+        .map(|line| {
+            line.split(" -> ")
+                .map(|coord| {
+                    let mut vec = coord
+                        .split(',')
+                        .map(|number| number.parse::<i32>().unwrap());
 
-                        (vec.next().unwrap(), vec.next().unwrap())
-                    })
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
+                    (vec.next().unwrap(), vec.next().unwrap())
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
 
-        let max_depth = cave
-            .iter()
-            .map(|line| line.iter().map(|coord| coord.1).max().unwrap())
-            .max()
-            .unwrap();
+    let max_depth = cave
+        .iter()
+        .map(|line| line.iter().map(|coord| coord.1).max().unwrap())
+        .max()
+        .unwrap();
 
-        let mut occupied_tiles = HashSet::new();
-        cave.iter().for_each(|line| {
-            line.windows(2).for_each(|pair| {
-                let (a, b) = (pair[0], pair[1]);
+    let mut occupied_tiles = HashSet::new();
+    cave.iter().for_each(|line| {
+        line.windows(2).for_each(|pair| {
+            let (a, b) = (pair[0], pair[1]);
 
-                if a.0 == b.0 {
-                    let from = a.1.min(b.1);
-                    let to = a.1.max(b.1);
+            if a.0 == b.0 {
+                let from = a.1.min(b.1);
+                let to = a.1.max(b.1);
 
-                    for i in from..=to {
-                        occupied_tiles.insert((a.0, i));
-                    }
-                } else if a.1 == b.1 {
-                    let from = a.0.min(b.0);
-                    let to = a.0.max(b.0);
-
-                    for i in from..=to {
-                        occupied_tiles.insert((i, a.1));
-                    }
-                } else {
-                    panic!("2 points are not alligned {a:?} {b:?}");
+                for i in from..=to {
+                    occupied_tiles.insert((a.0, i));
                 }
-            })
-        });
+            } else if a.1 == b.1 {
+                let from = a.0.min(b.0);
+                let to = a.0.max(b.0);
 
-        let floor = max_depth + 2;
-        let mut counter = 0;
-        let start_point = (500, 0);
-        let mut sand = start_point;
-        loop {
-            if occupied_tiles.contains(&(500, 0)) {
-                break;
+                for i in from..=to {
+                    occupied_tiles.insert((i, a.1));
+                }
+            } else {
+                panic!("2 points are not alligned {a:?} {b:?}");
             }
+        })
+    });
 
-            if sand.1 + 1 == floor {
-                occupied_tiles.insert((sand.0, sand.1 + 1));
-                occupied_tiles.insert((sand.0 - 1, sand.1 + 1));
-                occupied_tiles.insert((sand.0 + 1, sand.1 + 1));
-            }
-
-            if !occupied_tiles.contains(&(sand.0, sand.1 + 1)) {
-                sand = (sand.0, sand.1 + 1);
-                continue;
-            }
-            if !occupied_tiles.contains(&(sand.0 - 1, sand.1 + 1)) {
-                sand = (sand.0 - 1, sand.1 + 1);
-                continue;
-            }
-            if !occupied_tiles.contains(&(sand.0 + 1, sand.1 + 1)) {
-                sand = (sand.0 + 1, sand.1 + 1);
-                continue;
-            }
-
-            counter += 1;
-            occupied_tiles.insert(sand);
-            sand = start_point;
+    let floor = max_depth + 2;
+    let mut counter = 0;
+    let start_point = (500, 0);
+    let mut sand = start_point;
+    loop {
+        if occupied_tiles.contains(&(500, 0)) {
+            break;
         }
 
-        println!("Sand count: {counter}")
+        if sand.1 + 1 == floor {
+            occupied_tiles.insert((sand.0, sand.1 + 1));
+            occupied_tiles.insert((sand.0 - 1, sand.1 + 1));
+            occupied_tiles.insert((sand.0 + 1, sand.1 + 1));
+        }
+
+        if !occupied_tiles.contains(&(sand.0, sand.1 + 1)) {
+            sand = (sand.0, sand.1 + 1);
+            continue;
+        }
+        if !occupied_tiles.contains(&(sand.0 - 1, sand.1 + 1)) {
+            sand = (sand.0 - 1, sand.1 + 1);
+            continue;
+        }
+        if !occupied_tiles.contains(&(sand.0 + 1, sand.1 + 1)) {
+            sand = (sand.0 + 1, sand.1 + 1);
+            continue;
+        }
+
+        counter += 1;
+        occupied_tiles.insert(sand);
+        sand = start_point;
     }
+
+    println!("Sand count: {counter}")
 }
 
 #[allow(dead_code)]
