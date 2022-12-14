3daf677c734d27f405d67abed4c75cf6b0709794
Son Luong Ngoc <sluongng@gmail.com>
2022-12-14 09:30:33 +0100

    d14: print the cave!

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,53 +1,80 @@
+use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     let input1 = fs::read_to_string("src/input1.txt").unwrap();
     let input2 = fs::read_to_string("src/input2.txt").unwrap();
 
-    let cave = input2
-        .as_str()
-        .trim()
-        .split('\n')
-        .map(|line| {
-            line.split(" -> ")
-                .map(|coord| {
-                    let mut vec = coord
-                        .split(',')
-                        .map(|number| number.parse::<i32>().unwrap());
+    for input in vec![input1] {
+        let cave = input
+            .as_str()
+            .trim()
+            .split('\n')
+            .map(|line| {
+                line.split(" -> ")
+                    .map(|coord| {
+                        let mut vec = coord
+                            .split(',')
+                            .map(|number| number.parse::<i32>().unwrap());
 
-                    (vec.next().unwrap(), vec.next().unwrap())
-                })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+                        (vec.next().unwrap(), vec.next().unwrap())
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
 
-    let widths = cave
-        .iter()
-        .map(|line| {
-            let horizontal_points = line.iter().map(|coord| coord.0);
+        let max_heights = cave
+            .iter()
+            .map(|line| line.iter().map(|coord| coord.1).max().unwrap())
+            .max()
+            .unwrap();
 
-            (
-                horizontal_points.clone().min().unwrap(),
-                horizontal_points.clone().max().unwrap(),
-            )
-        })
-        .fold((i32::MAX, 0), |(min, max), (current_min, current_max)| {
-            (min.min(current_min), max.max(current_max))
+        let mut occupied_tiles = HashSet::new();
+        cave.iter().for_each(|line| {
+            line.windows(2).for_each(|pair| {
+                let (a, b) = (pair[0], pair[1]);
+
+                println!("Connecting {a:?} - {b:?}");
+                if a.0 == b.0 {
+                    let from = a.1.min(b.1);
+                    let to = a.1.max(b.1);
+
+                    for i in from..=to {
+                        occupied_tiles.insert((a.0, i));
+                    }
+                } else if a.1 == b.1 {
+                    let from = a.0.min(b.0);
+                    let to = a.0.max(b.0);
+
+                    for i in from..=to {
+                        occupied_tiles.insert((i, a.1));
+                    }
+                } else {
+                    panic!("2 points are not alligned {a:?} {b:?}");
+                }
+            })
         });
-    let heights = cave
-        .iter()
-        .map(|line| {
-            let horizontal_points = line.iter().map(|coord| coord.1);
 
-            (
-                horizontal_points.clone().min().unwrap(),
-                horizontal_points.clone().max().unwrap(),
-            )
-        })
-        .fold((i32::MAX, 0), |(min, max), (current_min, current_max)| {
-            (min.min(current_min), max.max(current_max))
-        });
+        draw_cave(occupied_tiles.clone());
+    }
+}
 
-    println!("The cave widths {widths:?}");
-    println!("The cave heights {heights:?}");
+fn draw_cave(cave: HashSet<(i32, i32)>) {
+    let mut lines = vec![];
+    for i in 0..=9 {
+        let mut line = vec![];
+        for j in 494..=503 {
+            if cave.contains(&(j, i)) {
+                line.push("#");
+            } else {
+                line.push(" ");
+            }
+        }
+
+        lines.push(line);
+    }
+
+    for (i, l) in lines.iter().enumerate() {
+        println!("{i: >2} {:?}", l);
+    }
 }
