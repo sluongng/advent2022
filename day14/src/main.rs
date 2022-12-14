2941629e1a898476ed55d44d50b368705f4ed4e2
Son Luong Ngoc <sluongng@gmail.com>
2022-12-14 09:49:31 +0100

    day14: part 1

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let input1 = fs::read_to_string("src/input1.txt").unwrap();
     let input2 = fs::read_to_string("src/input2.txt").unwrap();
 
-    for input in vec![input1] {
+    for input in vec![input2] {
         let cave = input
             .as_str()
             .trim()
@@ -23,7 +23,7 @@ fn main() {
             })
             .collect::<Vec<_>>();
 
-        let max_heights = cave
+        let max_depth = cave
             .iter()
             .map(|line| line.iter().map(|coord| coord.1).max().unwrap())
             .max()
@@ -34,7 +34,6 @@ fn main() {
             line.windows(2).for_each(|pair| {
                 let (a, b) = (pair[0], pair[1]);
 
-                println!("Connecting {a:?} - {b:?}");
                 if a.0 == b.0 {
                     let from = a.1.min(b.1);
                     let to = a.1.max(b.1);
@@ -55,10 +54,37 @@ fn main() {
             })
         });
 
-        draw_cave(occupied_tiles.clone());
+        let mut counter = 0;
+        let start_point = (500, 0);
+        let mut sand = start_point;
+        loop {
+            if sand.1 > max_depth {
+                break;
+            }
+
+            if !occupied_tiles.contains(&(sand.0, sand.1 + 1)) {
+                sand = (sand.0, sand.1 + 1);
+                continue;
+            }
+            if !occupied_tiles.contains(&(sand.0 - 1, sand.1 + 1)) {
+                sand = (sand.0 - 1, sand.1 + 1);
+                continue;
+            }
+            if !occupied_tiles.contains(&(sand.0 + 1, sand.1 + 1)) {
+                sand = (sand.0 + 1, sand.1 + 1);
+                continue;
+            }
+
+            counter += 1;
+            occupied_tiles.insert(sand);
+            sand = start_point;
+        }
+
+        println!("Sand count: {counter}")
     }
 }
 
+#[allow(dead_code)]
 fn draw_cave(cave: HashSet<(i32, i32)>) {
     let mut lines = vec![];
     for i in 0..=9 {
