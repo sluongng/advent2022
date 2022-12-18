ac46335ddea4d6156e4ff9499e35979467c2a573
Son Luong Ngoc <sluongng@gmail.com>
2022-12-18 14:02:29 +0100

    d17: solved p1

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -60,30 +60,67 @@ impl Tall {
         }
     }
 
-    fn tick(&mut self) -> bool {
-        if self.rock_count == 0 {
-            self.add_rock();
-        }
+    // Use for debugging
+    fn render(&self) {
+        self.chamber.iter().enumerate().rev().for_each(|(j, line)| {
+            let line = line
+                .iter()
+                .enumerate()
+                .map(|(i, b)| {
+                    if self.falling_rock.contains(&(i, j)) {
+                        return "@";
+                    }
 
-        let h_move = self.future_moves.next().unwrap();
-        self.move_to_side(h_move);
-        if !self.move_down() {
+                    match b {
+                        true => "#",
+                        false => ".",
+                    }
+                })
+                .fold("".into(), |acc: String, s| acc + " " + s);
+
+            println!("{line}");
+        });
+        println!("");
+    }
+
+    fn tick(&mut self) -> bool {
+        if self.falling_rock.len() == 0 {
             self.add_rock();
             self.rock_count += 1;
         }
 
-        if self.rock_count < MAX_ROCK_COUNT {
+        let h_move = self.future_moves.next().unwrap();
+        self.move_to_side(h_move);
+        self.move_down();
+
+        if self.falling_rock.len() == 0 {
             // Trim empty lines at top because
             // we will be injecting them when we add new rocks.
             self.chamber = self
                 .chamber
                 .clone()
                 .into_iter()
-                .skip_while(|line| line.iter().all(|&cell| cell))
+                .rev()
+                .skip_while(|line| line.iter().all(|&cell| !cell))
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
                 .collect::<Vec<_>>();
-
+        }
+        if self.rock_count <= MAX_ROCK_COUNT {
             true
         } else {
+            self.chamber = self
+                .chamber
+                .clone()
+                .into_iter()
+                .rev()
+                .skip_while(|line| line.iter().all(|&cell| !cell))
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
+                .collect::<Vec<_>>();
+
             false
         }
     }
@@ -91,15 +128,11 @@ impl Tall {
     fn add_rock(&mut self) {
         self.chamber.push(vec![false; GAME_WIDTH]);
         self.chamber.push(vec![false; GAME_WIDTH]);
+        self.chamber.push(vec![false; GAME_WIDTH]);
 
         match self.future_rocks.next() {
             Some(RockKind::Line) => {
-                vec![
-                    //    0      1      2      3     4       5     6
-                    vec![false, false, true, true, true, true, false], // | . . # # # # . |
-                ]
-                .into_iter()
-                .for_each(|line| self.chamber.push(line));
+                self.chamber.push(vec![false; GAME_WIDTH]);
 
                 let current_height = self.chamber.len() - 1;
                 self.falling_rock = vec![
@@ -110,14 +143,9 @@ impl Tall {
                 ];
             }
             Some(RockKind::Plus) => {
-                vec![
-                    //    0      1      2      3     4       5     6
-                    vec![false, false, false, true, false, false, false], // | . . . # . . . |
-                    vec![false, false, true, true, true, false, false],   // | . . # # # . . |
-                    vec![false, false, false, true, false, false, false], // | . . . # . . . |
-                ]
-                .into_iter()
-                .for_each(|line| self.chamber.push(line));
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
 
                 let current_height = self.chamber.len() - 1;
                 self.falling_rock = vec![
@@ -129,18 +157,9 @@ impl Tall {
                 ];
             }
             Some(RockKind::RevEl) => {
-                vec![
-                    //    0      1      2      3     4       5     6
-                    vec![false, false, false, false, true, false, false], // | . . . . # . . |
-                    vec![false, false, false, false, true, false, false], // | . . . . # . . |
-                    vec![false, false, true, true, true, false, false],   // | . . # # # . . |
-                ]
-                .into_iter()
-                // We are inserting into the tall one-by-one
-                // so we need to push the bottom lines first,
-                // which means reverse ordering of the drawing.
-                .rev()
-                .for_each(|line| self.chamber.push(line));
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
 
                 let current_height = self.chamber.len() - 1;
                 self.falling_rock = vec![
@@ -152,15 +171,10 @@ impl Tall {
                 ];
             }
             Some(RockKind::StraightLine) => {
-                vec![
-                    //    0      1      2      3     4       5     6
-                    vec![false, false, true, false, false, false, false], // | . . # . . . . |
-                    vec![false, false, true, false, false, false, false], // | . . # . . . . |
-                    vec![false, false, true, false, false, false, false], // | . . # . . . . |
-                    vec![false, false, true, false, false, false, false], // | . . # . . . . |
-                ]
-                .into_iter()
-                .for_each(|line| self.chamber.push(line));
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
 
                 let current_height = self.chamber.len() - 1;
                 self.falling_rock = vec![
@@ -171,13 +185,8 @@ impl Tall {
                 ];
             }
             Some(RockKind::Square) => {
-                vec![
-                    //    0      1      2      3     4       5     6
-                    vec![false, false, true, true, false, false, false], // | . . # # . . . |
-                    vec![false, false, true, true, false, false, false], // | . . # # . . . |
-                ]
-                .into_iter()
-                .for_each(|line| self.chamber.push(line));
+                self.chamber.push(vec![false; GAME_WIDTH]);
+                self.chamber.push(vec![false; GAME_WIDTH]);
 
                 let current_height = self.chamber.len() - 1;
                 self.falling_rock = vec![
@@ -193,8 +202,8 @@ impl Tall {
 
     fn move_to_side(&mut self, direction: HDirection) {
         let (new_position, is_legal) = self.move_rock(match direction {
-            HDirection::Left => (1, 0),
-            HDirection::Right => (-1, 0),
+            HDirection::Left => (-1, 0),
+            HDirection::Right => (1, 0),
         });
 
         if is_legal {
@@ -205,7 +214,7 @@ impl Tall {
         }
     }
 
-    fn move_down(&mut self) -> bool {
+    fn move_down(&mut self) {
         let (new_position, is_legal) = self.move_rock((0, -1));
 
         if is_legal {
@@ -213,10 +222,12 @@ impl Tall {
                 .iter()
                 .map(|&(a, b)| (a as usize, b as usize))
                 .collect();
-
-            true
         } else {
-            false
+            self.falling_rock
+                .iter()
+                .for_each(|&(a, b)| self.chamber[b][a] = true);
+
+            self.falling_rock.clear();
         }
     }
 
@@ -229,28 +240,19 @@ impl Tall {
             .collect();
 
         // collision check
-        let is_legal = new_position
-            .iter()
-            .map(|&(a, b)| {
-                a >= 0
-                    && b >= 0
-                    && a < GAME_WIDTH as i32
-                    && b < GAME_WIDTH as i32
-                    && *self
-                        .chamber
-                        .get(b as usize)
-                        .unwrap()
-                        .get(a as usize)
-                        .unwrap()
-            })
-            .all(|b| b);
+        let is_legal = new_position.iter().all(|&(a, b)| {
+            let legal =
+                a >= 0 && b >= 0 && a < GAME_WIDTH as i32 && !self.chamber[b as usize][a as usize];
+
+            legal
+        });
 
         (new_position, is_legal)
     }
 }
 
 fn main() {
-    let mut tall = Tall::new(include_str!("input1.txt").trim());
+    let mut tall = Tall::new(include_str!("input2.txt").trim());
 
     while tall.tick() {
         // tick
